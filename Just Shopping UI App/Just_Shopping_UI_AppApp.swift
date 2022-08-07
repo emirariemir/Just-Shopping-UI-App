1e71157efa32adabc8fc3e8d37bc0368f27dc1ba
Emir Arı <emirariomatic@gmail.com>
2022-08-07 12:23:22 +0300

    Initial Commit

---
+++ Just Shopping UI App/Just_Shopping_UI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_Shopping_UI_AppApp.swift
+//  Just Shopping UI App
+//
+//  Created by Emir on 7.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Just_Shopping_UI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
